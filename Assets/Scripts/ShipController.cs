d30c5deba77e598b7e9427b0684916788ce8893a
Marc Breaux <marc@mbtechsolutions.com>
2015-08-24 23:34:06 -0400

    Imported assets.  Have ship that can fly around.  "Warp" script handles objects that move out of frame, and places them on the other side.

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipController : MonoBehaviour {
+
+    public float RotateSpeed = 360.0f;
+    public float Thrust = 0.0f;
+    public float ThrustPower = 5.0f;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        // Gather input for rotation
+        float rotatedir = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.back, rotatedir * RotateSpeed * Time.deltaTime);
+
+        // Gather input for thrust
+        if (Input.GetButton("Fire1")) Thrust = 1.0f;
+        else Thrust = 0.0f;
+	}
+
+    void FixedUpdate()
+    {
+        // Apply thrust to ship
+        rb.angularVelocity = 0.0f;
+        rb.AddRelativeForce(new Vector2(0.0f, Thrust * ThrustPower));
+    }
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Warp : MonoBehaviour {
+
+    Collider2D cd;
+
+    void Awake()
+    {
+        cd = GetComponent<Collider2D>();
+    }
+	// Update is called once per frame
+	void Update () {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        bool inCamera = false;
+        // Outside of the bound of the camera
+        Bounds cameraBounds = new Bounds(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0.0f), new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        if (cd.bounds.Intersects(cameraBounds)) inCamera = true;
+
+        if (!inCamera)
+        {
+            float maxx = transform.position.x + cd.bounds.extents.x;
+            float minx = transform.position.x - cd.bounds.extents.x;
+            float maxy = transform.position.y + cd.bounds.extents.y;
+            float miny = transform.position.y - cd.bounds.extents.y;
+            float newx = transform.position.x;
+            float newy = transform.position.y;
+            // Object is too far to the right
+            if (minx > cameraBounds.center.x + cameraBounds.extents.x)
+            {
+                newx = cameraBounds.center.x - cameraBounds.extents.x - cd.bounds.extents.x;
+            }
+            // Object is too far to the left
+            if (maxx < cameraBounds.center.x - cameraBounds.extents.x)
+            {
+                newx = cameraBounds.center.x + cameraBounds.extents.x + cd.bounds.extents.x;
+            }
+            // Object is too far to the top
+            if (miny > cameraBounds.center.y + cameraBounds.extents.y)
+            {
+                newy = cameraBounds.center.y - cameraBounds.extents.y - cd.bounds.extents.y;
+            }
+            // Object is too far to the bottom
+            if (maxy < cameraBounds.center.y - cameraBounds.extents.y)
+            {
+                newy = cameraBounds.center.y + cameraBounds.extents.y + cd.bounds.extents.y;
+            }
+            transform.position = new Vector3(newx, newy, 0.0f);
+        }
+	}
+}
